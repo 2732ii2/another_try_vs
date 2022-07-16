9ac3652b92203754e81fce02b8f23fd950da332a
Ashad Mubeen <mo.anas123khan@gmail.com>
2022-07-16 11:25:41 +0530

    again third commit in js

--- edit.js
+++ edit.js
@@ -1,1 +1,16 @@
 
+function ds()
+{
+    console.log('clicked!');
+    var a=document.getElementById('dh');
+    if (a.paused!=true)
+    {
+        a.pause();
+        console.log("true");
+    }
+    else{
+        a.play();
+        console.log("false");
+    }
+    
+}
